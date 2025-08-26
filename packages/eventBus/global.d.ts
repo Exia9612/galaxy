f3b418d5cc5c0385c7447b23aaa3fa1c36691726
Qipeng cheng <565644830@qq.com>
2025-08-26 23:43:41 +0800

    feat: use rollup build eventbus

---
+++ packages/eventBus/global.d.ts
@@ -0,0 +1,1 @@
+export type TSFunction<T> = (...args: any[]) => T;
--- packages/eventBus/src/eventBus.ts
+++ packages/eventBus/src/eventBus.ts
@@ -1,41 +1,54 @@
+import { TSFunction } from "../global";
+
 enum StringEvents {
 	USER_LOGIN = "user:login",
 	USER_LOGOUT = "user:logout",
 	DATA_UPDATE = "data:update",
 }
 
-// class EventBus<T extends string> {
-// 	// eventStore: Map<T, ((...args: [any]) => void)[]>;
-// 	eventStore: {
-// 		[key in T]: Function[];
-// 	};
+export class EventBus<T extends string> {
+	eventStore: {
+		[key in T]: TSFunction<void>[];
+	};
 
-// 	constructor() {
-// 		this.eventStore = {} as {
-// 			[key in T]: Function[];
-// 		};
-// 	}
+	constructor() {
+		this.eventStore = {} as {
+			[key in T]: TSFunction<void>[];
+		};
+	}
 
-// 	addEvent(event: T, callback: (...args: [any]) => void) {
-// 		if (!this.eventStore[event]) {
-// 			this.eventStore[event] = [callback];
-// 			return;
-// 		}
-// 		this.eventStore[event].push(callback);
-// 	}
+	subscribe(event: T, callback: TSFunction<void>) {
+		if (!this.eventStore[event]) {
+			this.eventStore[event] = [callback];
+			return;
+		}
+		this.eventStore[event].push(callback);
+	}
 
-// 	removeEvent(event: T, callback: Function) {
-// 		if (!this.eventStore[event]) {
-// 			return;
-// 		}
-// 		this.eventStore[event] = this.eventStore[event].filter(
-// 			(cb) => cb !== callback,
-// 		);
-// 	}
+	unsubscribe(event: T, callback: TSFunction<void>) {
+		if (!this.eventStore[event]) {
+			return;
+		}
+		this.eventStore[event] = this.eventStore[event].filter(
+			(cb) => cb !== callback,
+		);
+	}
 
-// 	emitEvent(event: T) {
-// 		this.eventStore[event].forEach((event) => event());
-// 	}
-// }
+	once(event: T, callback: TSFunction<void>) {
+		const wrapper = () => {
+			callback();
+			this.unsubscribe(event, callback);
+		};
+		if (!this.eventStore[event]) {
+			this.eventStore[event] = [wrapper];
+			return;
+		}
+		this.eventStore[event].push(wrapper);
+	}
 
-// new EventBus<StringEvents>();
+	publish(event: T) {
+		this.eventStore[event].forEach((event) => event());
+	}
+}
+
+new EventBus<StringEvents>();
